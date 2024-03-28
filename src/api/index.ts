2c00c3f833f8e84919964aef7925b5a21676b748
Vlad Solovey <connectwithvlad@gmail.com>
2024-03-29 00:40:01 +0300

    feat: setup axios instance

---
+++ src/api/index.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_RAPID_API_URL,
+  timeout: 1000,
+  headers: {
+    'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+    'X-RapidAPI-Host': import.meta.env.VITE_RAPID_API_HOST,
+  }
+})
+
+export const getBeersByCountry = (country: string) => {
+  return api.get(country);
+}
+
+export default api;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_RAPID_API_KEY: string;
+  readonly VITE_RAPID_API_HOST: string;
+  readonly VITE_RAPID_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
